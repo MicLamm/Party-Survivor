9f6fa6106c1882b1ea8fc03778aeafe9e7f6dd66
Michael Lambert <micxxlamb@outlook.com>
2021-11-10 16:17:29 -0500

    Added search bar, created ChoixCategorie,choixFavorie and CreationItem

--- app/src/main/java/uqac/dim/partysurvivor/ChoixJeu.kt
+++ app/src/main/java/uqac/dim/partysurvivor/ChoixJeu.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.SearchView
 import android.widget.TextView
+import androidx.core.view.children
 
 class ChoixJeu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,8 @@ class ChoixJeu : AppCompatActivity() {
         for(i in 1..5){
             val row: View = lInflater.inflate(R.layout.row,view,false)
             val rowbtn: Button = row.findViewById(R.id.GameBtn)
+            val rowIndex: TextView = row.findViewById(R.id.Index)
+            rowIndex.text = "1"
             rowbtn.setOnClickListener {
                 //val intent = Intent(v.context, ItemProfiler::class.java)
                 //val tv1 = row.findViewById<View>(R.id.item_hash) as TextView
@@ -38,5 +44,48 @@ class ChoixJeu : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu,menu)
+
+        val search: MenuItem? = menu?.findItem(R.id.nav_search)
+        val searchView: SearchView = search?.actionView as SearchView
+        searchView.queryHint = "Search something"
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                var view:LinearLayout = findViewById(R.id.MainLayout)
+                for (childView in view.children){
+                    val btn: Button = childView.findViewById(R.id.GameBtn)
+                    if (!btn.text.startsWith(newText.toString())){
+                        btn.visibility =View.GONE
+                    }
+                    else{
+                        btn.visibility =View.VISIBLE
+                    }
+
+                }
+                view = findViewById(R.id.OptionalLayout)
+                for (childView in view.children){
+                    val btn: Button = childView.findViewById(R.id.GameBtn)
+                    if (!btn.text.startsWith(newText.toString())){
+                        btn.visibility =View.GONE
+                    }
+                    else{
+                        btn.visibility =View.VISIBLE
+                    }
+
+                }
+                return true
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
---
+++ app/src/main/java/uqac/dim/partysurvivor/CreationItem.kt
@@ -0,0 +1,11 @@
+package uqac.dim.partysurvivor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreationItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creation_item)
+    }
+}
